0d7bda09b4e8f728789555f8ff06b341a379f780
Nico <nico@192.168.1.8>
2024-02-22 15:16:27 -0300

    setup ios project

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import AppNavigator from './src/screens';
 import {QueryClientProvider, QueryClient} from '@tanstack/react-query';
+import {SafeAreaView} from 'react-native';
+import Colors from './src/types/colors';
 
 const queryClient = new QueryClient();
 
 function App(): React.JSX.Element {
   return (
     <QueryClientProvider client={queryClient}>
-      <AppNavigator />
+      <SafeAreaView style={{backgroundColor: Colors.black, flex: 1}}>
+        <AppNavigator />
+      </SafeAreaView>
     </QueryClientProvider>
   );
 }
